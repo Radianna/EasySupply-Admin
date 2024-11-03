292e8bdcebade2768e480ddf8bcfae238f840511
Radianna <radityaannabil934@gmail.com>
2024-11-03 10:23:52 +0700

    migration

--- database/migrations/2024_10_24_052214_create_pesanans_table.php
+++ database/migrations/2024_10_24_052214_create_pesanans_table.php
@@ -1,1 +1,25 @@
- 
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up()
+    {
+        Schema::create('pesanans', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->date('tanggal_pesanan');
+            $table->integer('total_harga');
+            $table->enum('status_pesanan', ['diproses', 'dikirim', 'selesai']);
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('pesanans');
+    }
+};
